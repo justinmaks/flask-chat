7c8ef9542d16a5e857248c82295af0cd72dad8ef
stn <justinmaks79@gmail.com>
2023-09-08 01:36:24 -0400

    optimize

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # flask-chat
+
+Use venv:
+
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+
+sqlite db
+
+deactivate
+
+
+
+
+
+TODO:
+- add better user/login/register error handling
+- add base.html with modern theme and modify templates to utilize and extend it
--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Suppress the modification tracking warning
+app.config.update(
+    SECRET_KEY='supersecretkey',
+    SQLALCHEMY_DATABASE_URI='sqlite:///database.db',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False  # Suppress the modification tracking warning
+)
 
-csrf = CSRFProtect(app)  # Initialize CSRF protection
-
+csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,10 +23,12 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(150), nullable=False)
     messages = db.relationship('Message', backref='author', lazy=True)
 
+
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(500), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -32,16 +36,15 @@ def load_user(user_id):
 
 
 @app.after_request
-def add_csp(response):
-    # This is a very strict policy 
-    # It allows only scripts from the same origin and inline styles.
-    # You can modify this according to your requirements.
+def secure_headers(response):
     response.headers["Content-Security-Policy"] = "default-src 'self'; style-src 'self' 'unsafe-inline'"
     return response
 
+
 @app.errorhandler(404)
-def not_found(e):
+def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -53,6 +56,7 @@ def register():
         return redirect(url_for('shoutbox'))
     return render_template('register.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -62,11 +66,13 @@ def login():
             return redirect(url_for('shoutbox'))
     return render_template('login.html')
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('shoutbox'))
+
 
 @app.route('/', methods=['GET', 'POST'])
 @login_required
@@ -75,9 +81,10 @@ def shoutbox():
         new_message = Message(content=request.form['message'], user_id=current_user.id)
         db.session.add(new_message)
         db.session.commit()
-        return redirect(url_for('shoutbox'))  # Redirect after POST to prevent form resubmission
-    messages = Message.query.order_by(Message.id.desc()).all()  # Ordering messages so latest messages come first
+        return redirect(url_for('shoutbox'))
+    messages = Message.query.order_by(Message.id.desc()).all()
     return render_template('shoutbox.html', messages=messages)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
