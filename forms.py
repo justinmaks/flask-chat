54dffa9b2bc516dcf1b6e20a2e0cb9ec88d7bdc9
justinmaks79@gmail.com <justinmaks79@gmail.com>
2023-09-11 01:47:01 -0400

    use flask-wtf forms

---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import TextAreaField, validators
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class ShoutboxForm(FlaskForm):
+    message = TextAreaField('Message', [
+        validators.Length(min=1, max=500), 
+        validators.DataRequired()
+    ])
